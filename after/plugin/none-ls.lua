caae7f4bf344600ed8adafde8e12ef3cf1c9eaa7
Rafael Monteiro Porto <rafael.monteiro.porto@outlook.com>
2024-01-29 11:03:38 -0300

    feat(go): Configurando lint e formatter

--- after/plugin/none-ls.lua
+++ after/plugin/none-ls.lua
@@ -1,12 +1,14 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.clj_kondo,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.codespell,
-        null_ls.builtins.formatting.sqlfmt
-    },
+	sources = {
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.diagnostics.clj_kondo,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.codespell,
+		null_ls.builtins.formatting.sqlfmt,
+		null_ls.builtins.formatting.goimports,
+	},
 })
